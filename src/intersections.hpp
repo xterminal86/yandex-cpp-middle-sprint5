bbc40e09cccf3205d4632ddb3630a2989f462a87
xterminal86 <xterminal86@gmail.com>
2026-06-16 18:33:38 +0300

    - added intersections

--- src/intersections.hpp
+++ src/intersections.hpp
@@ -19,7 +19,45 @@ namespace geometry::intersections {
 class IntersectionVisitor
 {
   public:
-    /* ваш код здесь */
+    bool operator()(const Line& l1, const Line& l2)
+    {
+      return true;
+    }
+
+    bool operator()(const Line& l, const Circle& c)
+    {
+      Point2D v1 = l.start - c.center_p;
+      Point2D v2 = l.end - l.start;
+
+      double area = v1.Cross(v2);
+
+      double perpendicular = area / v2.Length();
+
+      return perpendicular < c.radius;
+    }
+
+    bool operator()(const Circle& c, const Line& l)
+    {
+      return operator()(l, c);
+    }
+
+    bool operator()(const Circle& c1, const Circle& c2)
+    {
+      double distanceVector = (c1.center_p - c2.center_p).Length();
+      double radSum         = c1.radius + c2.radius;
+
+      return (distanceVector < radSum);
+    }
+
+    template <typename T, typename U>
+    bool operator()(const T& a, const U& b)
+    {
+      throw std::logic_error(
+        std::format("IntersectionVisitor() incompatible types: '{}' and '{}'",
+                    typeid(T).name(),
+                    typeid(U).name())
+      );
+    }
 };
 
 inline std::optional<Point2D> GetIntersectPoint(const Shape& shape1,
